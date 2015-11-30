267e39b403f5be8c13028f44e07d6c8ea86af654
stephane <stephane.bolu@gmail.com>
2015-11-30 11:22:17 +0100

    factorisation of the code

--- Cleansing/team_writting.py
+++ Cleansing/team_writting.py
@@ -12,24 +12,19 @@ db = client['cleansed']
 
 team_names = ['Man United', 'Man City','Arsenal','Chelsea','Liverpool','West Ham','Leicester','Everton','Swansea','Crystal Palace','Tottenham','Watford','Norwich City','West Brom','Bournemouth','Southampton','Aston Villa','Stoke','Newcastle','Sunderland']
 
+def file_write(team_and_result, collec):
+	with open(team.lower()+"_win.txt", "w") as f:
+		for tweet in collec.find():
+			for word in tweet['words']:
+				print(word+" ", file=f)
+	return
 
 for team in team_names:
 	collection_win = db[team.lower()+'_win']
 	collection_lose = db[team.lower()+'_lose']
 	collection_draw = db[team.lower()+'_draw']
 
-	with open(team.lower()+"_win.txt", "w") as f1:
-		for tweet in collection_win.find():
-			for word in tweet['words']:
-				print(word+" ", file=f1)
-
-	with open(team.lower()+"_lose.txt", "w") as f2:
-		for tweet in collection_lose.find():
-			for word in tweet['words']:
-				print(word+" ", file=f2)
-
-	with open(team.lower()+"_draw.txt", "w") as f3:
-		for tweet in collection_draw.find():
-			for word in tweet['words']:
-				print(word+" ", file=f3)
+	file_write(team.lower()+"_win.txt", collection_win)
+	file_write(team.lower()+"_lose.txt", collection_lose)
+	file_write(team.lower()+"_draw.txt", collection_draw)
 
