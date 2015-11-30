1bd61652eeb40d2166741e9a0e7bebec06491350
stephane <stephane.bolu@gmail.com>
2015-11-30 10:33:58 +0100

    bug fix team-writting

--- Cleansing/team_writting.py
+++ Cleansing/team_writting.py
@@ -10,7 +10,7 @@ client = MongoClient()
 # Connection to the database
 db = client['cleansed']
 
-team_names = ['Man United', 'Man City''Arsenal','Chelsea','Liverpool','West Ham','Leicester','Everton','Swansea','Crystal Palace','Tottenham','Watford','Norwich City','West Brom','Bournemouth','Southampton','Aston Villa','Stoke','Newcastle','Sunderland',]
+team_names = ['Man United', 'Man City','Arsenal','Chelsea','Liverpool','West Ham','Leicester','Everton','Swansea','Crystal Palace','Tottenham','Watford','Norwich City','West Brom','Bournemouth','Southampton','Aston Villa','Stoke','Newcastle','Sunderland']
 
 
 for team in team_names:
@@ -18,18 +18,18 @@ for team in team_names:
 	collection_lose = db[team.lower()+'_lose']
 	collection_draw = db[team.lower()+'_draw']
 
-	with open(team.lower()+"_win.txt", "w") as f:
+	with open(team.lower()+"_win.txt", "w") as f1:
 		for tweet in collection_win.find():
 			for word in tweet['words']:
-				print(word+" ", file=f)
+				print(word+" ", file=f1)
 
-	with open(team.lower()+"_lose.txt", "w") as f:
+	with open(team.lower()+"_lose.txt", "w") as f2:
 		for tweet in collection_lose.find():
 			for word in tweet['words']:
-				print(word+" ", file=f)
+				print(word+" ", file=f2)
 
-	with open(team.lower()+"_draw.txt", "w") as f:
+	with open(team.lower()+"_draw.txt", "w") as f3:
 		for tweet in collection_draw.find():
 			for word in tweet['words']:
-				print(word+" ", file=f)
+				print(word+" ", file=f3)
 
